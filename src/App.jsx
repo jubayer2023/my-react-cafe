cfd25bfdcac991e94916a683266ba05994cd116d
jubayer2023 <jubayersarker400@gmail.com>
2023-09-12 20:27:25 +0600

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl text-center text-black font-bold mt-5'>My-Ract-cafe</h1>
+    </>
+  )
+}
+
+export default App
